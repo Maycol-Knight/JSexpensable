80069e57113ece282ea51f77028649d07e67cc7e
Maycol-Knight <maycolquispecaccha@gmail.com>
2026-03-10 14:11:15 -0500

    Add render expenses

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// console.log("Hello World!");
+//Data store
+const expenses = [
+  {
+    category: "Shopping",
+    description: "Nintendo Switch!!",
+    amount: "500",
+  },
+  {
+    category: "Education",
+    description: "DevTalles",
+    amount: "300",
+  },
+];
+
+function createExpensesEl(expense) {
+  //Create elements
+  const li = document.createElement("li");
+  const expenseDetail = document.createElement("div");
+  const container = document.createElement("div");
+  const category = document.createElement("p");
+  const description = document.createElement("p");
+  const amount = document.createElement("p");
+  const actions = document.createElement("div");
+  const deleteLink = document.createElement("a");
+  //Properties of the elements (class,content)
+  li.classList.add("expense");
+  expenseDetail.classList.add("expense__detail");
+  category.className = "heading--xs bold";
+  category.textContent = expense.category;
+  description.className = "conten-sm gray-300";
+  description.textContent = expense.description;
+  amount.className = "content-xl bold";
+  amount.textContent = `$${expense.amount}`;
+  actions.classList.add("expense__actions");
+  deleteLink.textContent = "Delete";
+  //Build template
+  container.append(category, description);
+  actions.append(deleteLink);
+  expenseDetail.append(container, amount);
+  li.append(expenseDetail, actions);
+  return li;
+}
+function renderExpenses(expenses) {
+  const expenseList = document.querySelector(".js-expenses");
+  expenseList.innerHTML = "";
+  expenses.forEach((expense) => {
+    const expenseEl = createExpensesEl(expense);
+    expenseList.append(expenseEl);
+  });
+}
+renderExpenses(expenses);
