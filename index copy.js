aaecf4e1942a4cd0356750700f054bd11e2a6731
maycol <maycolquispecaccha@gmail.com>
2026-03-11 06:42:53 -0500

    Add example module pattern

---
+++ index copy.js
@@ -0,0 +1,44 @@
+renderExpenses(expenses);
+function createExpensesEl(expense) {
+  //Create elements
+  const li = document.createElement("li");
+  const expenseDetail = document.createElement("div");
+  const container = document.createElement("div");
+  const category = document.createElement("p");
+  const description = document.createElement("p");
+  const amount = document.createElement("p");
+  const actions = document.createElement("div");
+  const deleteLink = document.createElement("a");
+  //Properties of the elements (class,content)
+  li.classList.add("expense");
+  expenseDetail.classList.add("expense__detail");
+  category.className = "heading--xs bold";
+  category.textContent = expense.category;
+  description.className = "conten-sm gray-300";
+  description.textContent = expense.description;
+  amount.className = "content-xl bold";
+  amount.textContent = `$${expense.amount}`;
+  actions.classList.add("expense__actions");
+  deleteLink.textContent = "Delete";
+  //Build template
+  container.append(category, description);
+  actions.append(deleteLink);
+  expenseDetail.append(container, amount);
+  li.append(expenseDetail, actions);
+  //Add Event Listeners
+  deleteLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    deleteExpense(expense);
+    renderExpenses(expenses);
+  });
+  return li;
+}
+function renderExpenses(expenses) {
+  const expenseList = document.querySelector(".js-expenses");
+  expenseList.innerHTML = "";
+  expenses.forEach((expense) => {
+    const expenseEl = createExpensesEl(expense);
+    expenseList.append(expenseEl);
+  });
+}
+
--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-renderExpenses(expenses);
-function createExpensesEl(expense) {
-  //Create elements
-  const li = document.createElement("li");
-  const expenseDetail = document.createElement("div");
-  const container = document.createElement("div");
-  const category = document.createElement("p");
-  const description = document.createElement("p");
-  const amount = document.createElement("p");
-  const actions = document.createElement("div");
-  const deleteLink = document.createElement("a");
-  //Properties of the elements (class,content)
-  li.classList.add("expense");
-  expenseDetail.classList.add("expense__detail");
-  category.className = "heading--xs bold";
-  category.textContent = expense.category;
-  description.className = "conten-sm gray-300";
-  description.textContent = expense.description;
-  amount.className = "content-xl bold";
-  amount.textContent = `$${expense.amount}`;
-  actions.classList.add("expense__actions");
-  deleteLink.textContent = "Delete";
-  //Build template
-  container.append(category, description);
-  actions.append(deleteLink);
-  expenseDetail.append(container, amount);
-  li.append(expenseDetail, actions);
-  //Add Event Listeners
-  deleteLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    deleteExpense(expense);
-    renderExpenses(expenses);
-  });
-  return li;
-}
-function renderExpenses(expenses) {
-  const expenseList = document.querySelector(".js-expenses");
-  expenseList.innerHTML = "";
-  expenses.forEach((expense) => {
-    const expenseEl = createExpensesEl(expense);
-    expenseList.append(expenseEl);
-  });
-}
+const DOMHandler = function(parentElement){
+  const parent = document.querySelector(parentElement);
+  if(!parent) throw new Error("Parent not found");
+  return{
+    load(module) {
+      parent.innerHTML = module;
+    },
+  };
+};
+const App = DOMHandler("#root");
+console.log(App);
 
+//App.load("<h1>Happy Hacking</h1>")
+
+//Create module
+function createHeader(){
+  const template = "<h1>Header</h1>"
+  return {
+    render(){
+      return template;
+    } 
+  }
+}
+const Header = createHeader();
+App.load(Header.render());
